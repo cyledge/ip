5bd26b331a8710af5109eab302091b7eb387b048
ScottChoi <e1095619@u.nus.edu>
2026-01-23 13:49:00 +0800

    Add Mark command

--- src/main/java/CYbot.java
+++ src/main/java/CYbot.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CYbot {
     private static String myName = "CYbot";
-    private static ArrayList<String> strList = new ArrayList<String>();
+    private static ArrayList<Task> taskList = new ArrayList<Task>();
+    private static ArrayList<String> cmdList = new ArrayList<String>(Arrays.asList("list", "mark", "unmark"));
 
     private static void printHorizontalLine() {
         System.out.println("____________________________________________________________");
@@ -22,24 +24,70 @@ public class CYbot {
     }
 
     private static void printList() {
-        for (int i = 1; i <= strList.toArray().length; i++) {
-            System.out.println(i + ". " + strList.get(i-1));
+        for (int i = 1; i <= taskList.toArray().length; i++) {
+            System.out.println(i + ". " + taskList.get(i-1));
         }
     }
 
-    private static void echoMsg(Scanner scanner) {
+    private static void addTask(String task) {
+        taskList.add(new Task(task));
+        System.out.println("added: " + task);
+    }
+
+    private static boolean isCommand(String userInput) {
+        String[] inputStr = userInput.split("\\s+");
+        if (cmdList.contains(inputStr[0])) {
+            return true;
+        }
+        return false;
+    }
+
+    private static String[] breakInput(String command) {
+        return command.split("\\s+");
+    }
+
+    private static void mark(int index) {
+        taskList.get(index).markDone();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(taskList.get(index));
+    }
+
+    private static void unmark(int index) {
+        taskList.get(index).unmarkDone();
+        System.out.println("Ok, I've marked this task as not done yet:");
+        System.out.println(taskList.get(index));
+    }
+
+    private static void callCommand(String userInput) {
+        String[] wholeCmd = breakInput(userInput);
+        String command = wholeCmd[0];
+        switch (command) {
+            case "list":
+                printList();
+                break;
+            case "mark":
+                mark(Integer.parseInt(wholeCmd[1]) - 1);
+                break;
+            case "unmark":
+                unmark(Integer.parseInt(wholeCmd[1]) - 1);
+                break;
+
+        }
+    }
+
+    private static void processMsg(Scanner scanner) {
         while (true) {
             String userInput = scanner.nextLine();
             if (userInput.equals("bye")) {
                 break;
             }
             printHorizontalLine();
-            if (userInput.equals("list")) {
-                printList();
-                continue;
+            if (isCommand(userInput)) {
+                callCommand(userInput);
+            } else {
+                addTask(userInput);
             }
-            strList.add(userInput);
-            System.out.println("added: " + userInput);
+
             printHorizontalLine();
         }
     }
@@ -47,7 +95,7 @@ public class CYbot {
     public static void main(String[] args) {
     welcomeMsg();
     Scanner scanner = new Scanner(System.in);
-    echoMsg(scanner);
+    processMsg(scanner);
 
     byeMsg();
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+public class Task {
+    private String name;
+    private boolean done;
+
+    Task(String name) {
+        this.name = name;
+        this.done = false;
+    }
+
+    public void markDone() {
+        this.done = true;
+    }
+
+    public void unmarkDone() {
+        this.done = false;
+    }
+
+    public String toString() {
+        String box;
+        if (done) {
+            box = "[X] ";
+        } else {
+            box = "[ ] ";
+        }
+        return box + name;
+    }
+}
