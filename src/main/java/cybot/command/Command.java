626ba38a69489955254b03b3446633965fce8fc5
ScottChoi <e1095619@u.nus.edu>
2026-02-10 19:11:45 +0800

    Modify showTaskList

--- src/main/java/cybot/command/Command.java
+++ src/main/java/cybot/command/Command.java
@@ -17,18 +17,19 @@ public abstract class Command {
     }
 
     protected static String showTaskList(TaskList tasks) {
+        String output = "Here are the tasks in your list:\n";
+        return showTaskList(tasks, output);
+    }
+
+    protected static String showTaskList(TaskList tasks, String outputString) {
         if (tasks.size() == 0) {
             return "No task in your list!\n";
         }
         StringBuilder sb = new StringBuilder();
-        sb.append("Here are the tasks in your list:\n");
+        sb.append(outputString);
         for (int i = 1; i <= tasks.size(); i++) {
             sb.append(i + ". " + tasks.get(i - 1) + "\n");
         }
         return sb.toString();
-    }
-
-    protected static String showTaskList(TaskList tasks, String outputString) {
-        return outputString + showTaskList(tasks);
     }
 }
